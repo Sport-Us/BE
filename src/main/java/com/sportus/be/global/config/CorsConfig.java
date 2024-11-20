06f3c638f3d8c81495e25c9a4764ab2688148953
leedy3838 <leedy3838@naver.com>
2024-11-20 22:51:27 +0900

    fix: cors 설정 수정

--- src/main/java/com/sportus/be/global/config/CorsConfig.java
+++ src/main/java/com/sportus/be/global/config/CorsConfig.java
@@ -28,9 +28,8 @@ public class CorsConfig {
                 HttpMethod.PATCH.name(),
                 HttpMethod.OPTIONS.name()));
         config.setAllowedHeaders(List.of("*"));
-        config.setExposedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Authorization", "Set-Cookie", "REFRESH_TOKEN"));
         config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
