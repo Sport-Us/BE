996a4884a835777ef0f75d22fe5f0c96814fa6ea
leedy3838 <leedy3838@naver.com>
2024-11-27 12:58:04 +0900

    feat: cardnews 더미데이터 추가

--- src/main/java/com/sportus/be/cardnews/repository/init/CardNewsInitializer.java
+++ src/main/java/com/sportus/be/cardnews/repository/init/CardNewsInitializer.java
@@ -32,41 +32,36 @@ public class CardNewsInitializer implements ApplicationRunner {
             List<CardNews> cardNewsList = new ArrayList<>();
 
             CardNews cardNews1 = CardNews.builder()
-                    .title("폭염에 알아두면 좋은 무더위쉼터에 관한 모든것")
+                    .title("생애주기 운동백서 - 5세 이상 어린이")
                     .build();
 
             CardNews cardNews2 = CardNews.builder()
-                    .title("우리 동네 버스정류장, 일상 속 작은 쉼터")
+                    .title("생애주기 운동백서 - 만성 질환이 있는 겅인과 노인")
                     .build();
 
             CardNews cardNews3 = CardNews.builder()
-                    .title("폭염대비 온열질환 예방을 위한 이행 가이드 안내")
+                    .title("생애주기 운동백서 - 신체적 장애를 가진 성인")
                     .build();
 
             CardNews cardNews4 = CardNews.builder()
-                    .title("무더운 여름 온열질환을 조심하세요!")
+                    .title("생애주기 운동백서 - 성장기인 청소년")
                     .build();
 
             CardNews cardNews5 = CardNews.builder()
-                    .title("여름철 폭염 이렇게 대비하세요!")
+                    .title("생애주기 운동백서 - 임산부")
                     .build();
 
             CardNews cardNews6 = CardNews.builder()
-                    .title("겨울철, 한파가 예보된다면 한랭질환을 조심하세요!")
+                    .title("생애주기 운동백서 - 65세 이상 노인")
                     .build();
 
             CardNews cardNews7 = CardNews.builder()
-                    .title("겨울철 안전을 위협하는 한파와 폭설, 대처법은?")
+                    .title("생애주기 운동백서 - 활동량이 많은 어린이")
                     .build();
 
             CardNews cardNews8 = CardNews.builder()
-                    .title("추울 땐 뭐다? 한파 신호등!")
+                    .title("생애주기 운동백서 - 운동이 부족한 직장인")
                     .build();
-
-            CardNews cardNews9 = CardNews.builder()
-                    .title("한파 국민행동요령 안내")
-                    .build();
-
             cardNewsList.add(cardNews1);
             cardNewsList.add(cardNews2);
             cardNewsList.add(cardNews3);
@@ -75,77 +70,57 @@ public class CardNewsInitializer implements ApplicationRunner {
             cardNewsList.add(cardNews6);
             cardNewsList.add(cardNews7);
             cardNewsList.add(cardNews8);
-            cardNewsList.add(cardNews9);
 
             List<Card> cardList = new ArrayList<>();
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/1.jpg", cardNews1));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/2.jpg", cardNews1));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/3.jpg", cardNews1));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/4.jpg", cardNews1));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/5.jpg", cardNews1));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/swelter/6.jpg", cardNews1));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/1-1.png", cardNews1));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/1-2.png", cardNews1));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/1-3.png", cardNews1));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/1-4.png", cardNews1));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/1-5.png", cardNews1));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/smart/1.jpg", cardNews2));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/smart/2.jpg", cardNews2));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/smart/3.jpg", cardNews2));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/smart/4.jpg", cardNews2));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/shelter/smart/5.jpg", cardNews2));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/2-1.png", cardNews2));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/2-2.png", cardNews2));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/2-3.png", cardNews2));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/2-4.png", cardNews2));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/2-5.png", cardNews2));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/1.jpg", cardNews3));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/2.jpg", cardNews3));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/3.jpg", cardNews3));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/4.jpg", cardNews3));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/5.jpg", cardNews3));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/sick/6.jpg", cardNews3));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/3-1.png", cardNews3));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/3-2.png", cardNews3));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/3-3.png", cardNews3));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/3-4.png", cardNews3));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/3-5.png", cardNews3));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/1.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/2.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/3.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/4.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/5.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/6.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/7.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/8.jpg", cardNews4));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/solution/9.jpg", cardNews4));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/4-1.png", cardNews4));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/4-2.png", cardNews4));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/4-3.png", cardNews4));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/4-4.png", cardNews4));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/4-5.png", cardNews4));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%800.png", cardNews5));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%8001.png", cardNews5));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%8002.png", cardNews5));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%8003.png", cardNews5));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%8004.png", cardNews5));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/hot/countryside/%EB%82%B4%EC%A7%8005.png", cardNews5));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/5-1.png", cardNews5));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/5-2.png", cardNews5));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/5-3.png", cardNews5));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/5-4.png", cardNews5));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/5-5.png", cardNews5));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/1.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/2.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/3.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/4.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/5.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/6.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/7.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/8.jpg", cardNews6));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/disease/9.jpg", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-1.png", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-2.png", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-3.png", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-4.png", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-5.png", cardNews6));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/6-6.png", cardNews6));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/1.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/2.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/3.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/4.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/5.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/6.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/7.jpg", cardNews7));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/snow/8.jpg", cardNews7));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/7-1.png", cardNews7));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/7-2.png", cardNews7));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/7-3.png", cardNews7));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/7-4.png", cardNews7));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/7-5.png", cardNews7));
 
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/1.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/2.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/3.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/4.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/5.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/6.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/7.jpg", cardNews8));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/blinker/8.jpg", cardNews8));
-
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/solution/1.jpg", cardNews9));
-            cardList.add(new Card("https://" + bucket + ".s3.amazonaws.com/" + "/news/cold/solution/2.jpg", cardNews9));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/8-1.png", cardNews8));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/8-2.png", cardNews8));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/8-3.png", cardNews8));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/8-4.png", cardNews8));
+            cardList.add(new Card("https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/cardnews/8-5.png", cardNews8));
 
             cardNewsRepository.saveAll(cardNewsList);
             cardRepository.saveAll(cardList);
